95ad01b4a58d1d8ea309f1fa5a201c9f52515013
renukajalluri <renukajalluri20@gmail.com>
2023-01-22 12:39:34 +0530

    vercel_deployement

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const app = require("./app"); // the actual Express application
 const http = require("http");
+const config = require("./utils/config");
+
 // const logger = require('./utils/logger')
 const port = config.PORT || 3000;
 
